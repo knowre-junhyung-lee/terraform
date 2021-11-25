582614c02369430ee378e267f52a43dfd602872b
knowre-junhyung-lee <junhyung.lee@knowre.com>
2021-11-25 13:06:49 +0900

    vpc_setting

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.66.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-northeast-2"
+}
+
+module "vpc" {
+  source                    = "./modules/network"
+  vpc_cidr_block            = "10.10.0.0/16"
+  public_subnet_cidr_block  = "10.10.0.0/24"
+  private_subnet_cidr_block = "10.10.1.0/24"
+}
