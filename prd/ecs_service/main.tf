02d69029b0955b98835ba4adac4d36735afbbb04
“Knowre-junhyung-lee” <junhyung.lee@knowre.com>
2021-11-29 14:45:43 +0900

    ecs 테라폼 백엔드 누락

--- prd/ecs_service/main.tf
+++ prd/ecs_service/main.tf
@@ -1,18 +1,18 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "3.66.0"
-#     }
-#   }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.66.0"
+    }
+  }
 
-#    backend "remote" {
-#     organization = "jhlee5391"
-#     workspaces {
-#       name = "ecs_service"
-#     }
-#   }
-# }
+   backend "remote" {
+    organization = "jhlee5391"
+    workspaces {
+      name = "ecs_service"
+    }
+  }
+}
 
 provider "aws" {
   region     = "ap-northeast-2"
